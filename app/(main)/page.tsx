2ffe6da9c71d2ff5c1ed5dff79ac3ca70ed546f9
bs1109 <newmohib@gmail.com>
2024-02-04 12:59:22 +0600

    Filterable table added to deshboard

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,78 +4,161 @@
 import React, { useEffect, useState } from 'react';
 import { Button } from 'primereact/button';
 import { Column } from 'primereact/column';
-import { DataTable } from 'primereact/datatable';
+import { DataTable, DataTableFilterMeta } from 'primereact/datatable';
+import { InputText } from 'primereact/inputtext';
+import { FilterMatchMode, FilterOperator } from 'primereact/api';
 
-    //ProductService Type
-
-    type InventoryStatus = 'INSTOCK' | 'LOWSTOCK' | 'OUTOFSTOCK';
-    type Status = 'DELIVERED' | 'PENDING' | 'RETURNED' | 'CANCELLED';
-
-    type ProductOrder = {
-        id?: string;
-        productCode?: string;
-        date?: string;
-        amount?: number;
-        quantity?: number;
-        customer?: string;
-        status?: Status;
-    };
-    type Product = {
-        id?: string;
-        code?: string;
+//CustomerService Type
+type ICountryObject ={
+    name: string,
+    code: string
+}
+type Customer = {
+    id?: number;
+    name?: string;
+    country?: ICountryObject;
+    company?: string;
+    date: Date;
+    status?: string;
+    activity?: number;
+    balance?: number | string;
+    verified?: boolean;
+    amount?: number;
+    price?: number;
+    rating?: number;
+    image?: string;
+    orders?: Customer[];
+    inventoryStatus?: string;
+    representative: {
         name: string;
-        description: string;
-        image?: string;
-        price?: number;
-        category?: string;
-        quantity?: number;
-        inventoryStatus?: InventoryStatus;
-        rating?: number;
-        orders?: ProductOrder[];
-        [key: string]: string | string[] | number | boolean | undefined | ProductOrder[] | InventoryStatus;
+        image: string;
     };
+};
 
-    // This Functions is used for API call
-const getProductsSmall = async ()=> {
-    return fetch('/demo/data/products-small.json', { headers: { 'Cache-Control': 'no-cache' } })
+// for Customer table
+
+const getCustomersLarge= async ()=> {
+    return fetch('/demo/data/customers-large.json', { headers: { 'Cache-Control': 'no-cache' } })
         .then((res) => res.json())
-        .then((d) => d.data as Product[]);
+        .then((d) => d.data as Customer[]);
 }
 
 // Export this component
-
 const Dashboard = () => {
-    const [products, setProducts] = useState<Product[]>([]);
+    // for Customer
+    const [customers1, setCustomers1] = useState<Customer[]>([]);
+    const [filters1, setFilters1] = useState<DataTableFilterMeta>({});
+    const [loading1, setLoading1] = useState(true);
+    const [globalFilterValue1, setGlobalFilterValue1] = useState('');
+
+
+    // for Customer Table
+
+    const clearFilter1 = () => {
+        initFilters1();
+    };
+
+    const onGlobalFilterChange1 = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        let _filters1 = { ...filters1 };
+        (_filters1['global'] as any).value = value;
+
+        setFilters1(_filters1);
+        setGlobalFilterValue1(value);
+    };
+
+    const renderHeader1 = () => {
+        return (
+            <div className="flex justify-content-between">
+                <Button type="button" icon="pi pi-filter-slash" label="Clear" outlined onClick={clearFilter1} />
+                <span className="p-input-icon-left">
+                    <i className="pi pi-search" />
+                    <InputText value={globalFilterValue1} onChange={onGlobalFilterChange1} placeholder="Keyword Search" />
+                </span>
+            </div>
+        );
+    };
 
     useEffect(() => {
-        getProductsSmall().then((data) => setProducts(data));
+
+        getCustomersLarge().then((data) => {
+            setCustomers1(getCustomers(data));
+            setLoading1(false);
+        });
+
+        initFilters1();
     }, []);
 
-    const formatCurrency = (value: number) => {
-        return value?.toLocaleString('en-US', {
-            style: 'currency',
-            currency: 'USD'
+    const getCustomers = (data: Customer[]) => {
+        return [...(data || [])].map((d) => {
+            d.date = new Date(d.date);
+            return d;
         });
     };
 
+    const initFilters1 = () => {
+
+        setFilters1({
+            global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+            name: {
+                operator: FilterOperator.AND,
+                constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }]
+            },
+            'country.name': {
+                operator: FilterOperator.AND,
+                constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }]
+            },
+            representative: { value: null, matchMode: FilterMatchMode.IN },
+            date: {
+                operator: FilterOperator.AND,
+                constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }]
+            },
+            balance: {
+                operator: FilterOperator.AND,
+                constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }]
+            },
+            status: {
+                operator: FilterOperator.OR,
+                constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }]
+            },
+            activity: { value: null, matchMode: FilterMatchMode.BETWEEN },
+            verified: { value: null, matchMode: FilterMatchMode.EQUALS }
+        });
+        setGlobalFilterValue1('');
+    };
+
+    const header1 = renderHeader1();
     return (
         <div className="grid">
-            <div className="col-12 xl:col-6">
+            {/* For customers Table */}
+            <div className="col-12 xl:col-12">
                 <div className="card">
-                    <h5>Recent Sales</h5>
-                    <DataTable value={products} rows={5} paginator >
-                        <Column header="Image" body={(data) => <img className="shadow-2" src={`/demo/images/product/${data.image}`} alt={data.image} width="50" />} />
-                        <Column field="name" header="Name" sortable style={{ width: '35%' }} />
-                        <Column field="price" header="Price" sortable style={{ width: '35%' }} body={(data) => formatCurrency(data.price)} />
-                        <Column
-                            header="View"
-                            style={{ width: '15%' }}
-                            body={() => (<Button icon="pi pi-search" text />
-                            )}
-                        />
+                    <h5>Filter Menu</h5>
+                    <DataTable
+                        value={customers1}
+                        paginator
+                        className="p-datatable-gridlines"
+                        showGridlines
+                        rows={10}
+                        dataKey="id"
+                        filters={filters1}
+                        filterDisplay="menu"
+                        loading={loading1}
+
+                        emptyMessage="No customers found."
+                        header={header1}
+                    >
+                        <Column field="name" header="Name" filter filterPlaceholder="Search by name" style={{ minWidth: '12rem' }} />
+                        {/* <Column header="Country" filterField="country.name" style={{ minWidth: '12rem' }} body={countryBodyTemplate} filter filterPlaceholder="Search by country" filterClear={filterClearTemplate} filterApply={filterApplyTemplate} />
+                        <Column header="Date" filterField="date" dataType="date" style={{ minWidth: '10rem' }} body={dateBodyTemplate} filter filterElement={dateFilterTemplate} />
+                        <Column header="Balance" filterField="balance" dataType="numeric" style={{ minWidth: '10rem' }} body={balanceBodyTemplate} filter filterElement={balanceFilterTemplate} />
+                        <Column field="status" header="Status" filterMenuStyle={{ width: '14rem' }} style={{ minWidth: '12rem' }} body={statusBodyTemplate} filter filterElement={statusFilterTemplate} />
+                        <Column field="activity" header="Activity" showFilterMatchModes={false} style={{ minWidth: '12rem' }} body={activityBodyTemplate} filter filterElement={activityFilterTemplate} />
+                        <Column field="verified" header="Verified" dataType="boolean" bodyClassName="text-center" style={{ minWidth: '8rem' }} body={verifiedBodyTemplate} filter filterElement={verifiedFilterTemplate} /> */}
                     </DataTable>
                 </div>
             </div>
+
 
         </div>
     );
